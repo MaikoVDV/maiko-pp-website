8d9388aeb1fa0042785519e80612df5359f2a231
Pancake Protector <maikovdveen@gmail.com>
2021-12-08 12:33:18 +0100

    Added line breaks to cards. You can now use \n to add a line break.

--- client/src/reducers/cardReducer.js
+++ client/src/reducers/cardReducer.js
@@ -10,7 +10,7 @@ const initState = {
         {
             _id: "dummy card, not from the db",
             title: "Click me!",
-            desc: "Welcome to my Personal Project website! To begin, login with your Google account using the button in the top-right corner.\nNext, you can join some groups. Use the button in the top-left to enter a group ID.\nHere are some groups I made so you can check out my website as easily as possible: "
+            desc: "Welcome to my Personal Project website! To begin, login with your Google account using the button in the top-right corner.\nNext, you can join some groups. Use the button in the top-left to enter a group ID.\nHere are some groups I made so you can check out my website as easily as possible: \n617d8c6f1b0c1a50f95c006a\n61afaccaa6c37312d3a2df74"
         }],
     loading: false
 };
